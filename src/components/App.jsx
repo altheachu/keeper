59fcb654f732734f1b6fa5c333eaba1f5e42ea3b
cch <aehtla2012@gmail.com>
2024-08-03 11:02:18 +0800

    [IMP] render notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../note";
+
+function createNote(item){
+  return <Note key={item.key} title={item.title} content={item.content}/>
+}
 
 function App() {
   return (
   <div>
     <Header />
-    <Note />
+    {notes.map(createNote)}
     <Footer />
   </div>)
 }
